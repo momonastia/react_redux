12c9e07ac5aa295ef4f59d7dcb52629b69c88b7f
momonastia~ <anastasiatyrikova@gmail.com>
2023-04-29 23:57:48 +0200

    Feat: comments update added

--- src/SingleComment.js
+++ src/SingleComment.js
@@ -1,4 +1,6 @@
 import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { commentUpdate } from "./redux/actions";
 
 function SingleComment({ data }) {
   /* console.log("single comment props >", props); */
@@ -7,16 +9,28 @@ function SingleComment({ data }) {
 
   const { text, id } = data;
 
+  const dispatch = useDispatch();
+
+  const handleUpdate = (e) => {
+    e.preventDefault();
+    console.log("Submit >>> ", commentText);
+    dispatch(commentUpdate(commentText, id));
+  };
+
   useEffect(() => {
     if (text) {
       setCommentText(text);
     }
   }, [text]);
 
+  const handleInput = (e) => {
+    setCommentText(e.target.value);
+  };
+
   return (
-    <form className="comments-item">
+    <form onSubmit={handleUpdate} className="comments-item">
       <div className="comments-item-delete">&times;</div>
-      <input type="text" value={commentText} />
+      <input type="text" value={commentText} onChange={handleInput} />
       <input type="submit" hidden />
     </form>
   );
