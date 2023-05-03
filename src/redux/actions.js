0c08c0a5e3bd403b51d33e144176385a08176f8f
momonastia~ <anastasiatyrikova@gmail.com>
2023-05-03 18:18:29 +0200

    Feat: loader added

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -6,6 +6,8 @@ import {
   COMMENT_UPDATE,
   COMMENT_DELETE,
   COMMENTS_LOAD,
+  LOADER_DISPLAY_ON,
+  LOADER_DISPLAY_OFF,
 } from "./types";
 
 export function incrementLikes() {
@@ -48,15 +50,32 @@ export function commentDelete(id) {
   };
 }
 
+export function loaderOn() {
+  return {
+    type: LOADER_DISPLAY_ON,
+  };
+}
+
+export function loaderOff() {
+  return {
+    type: LOADER_DISPLAY_OFF,
+  };
+}
+
 export function commentsLoad() {
   return async (dispatch) => {
+    dispatch(loaderOn());
     const response = await fetch(
       "https://jsonplaceholder.typicode.com/comments?_limit=10"
     );
     const jsonData = await response.json();
-    dispatch({
-      type: COMMENTS_LOAD,
-      data: jsonData,
-    });
+
+    setTimeout(() => {
+      dispatch({
+        type: COMMENTS_LOAD,
+        data: jsonData,
+      });
+      dispatch(loaderOff());
+    }, 1000);
   };
 }
--- src/redux/commentsReducer.js
+++ src/redux/commentsReducer.js
@@ -10,7 +10,7 @@ const initialState = {
 };
 
 export const commentsReducer = (state = initialState, action) => {
-  console.log("comments Reducer >", action);
+  /* console.log("comments Reducer >", action); */
 
   switch (action.type) {
     case COMMENT_CREATE:
