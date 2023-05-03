a5d6a283ea78edab9fc891e5f9c9e17933ec4626
momonastia~ <anastasiatyrikova@gmail.com>
2023-05-03 18:43:37 +0200

    Feat: error handler added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import "./App.css";
+import { useSelector } from "react-redux";
 import Likes from "./Likes";
 import Title from "./Title";
 import Comments from "./Comments";
 import Spin from "./Spin";
 
 function App() {
+  const error = useSelector((state) => state.appReducer.error);
+
+  console.log("errro>>>>", error);
   return (
     <div className="App">
       <div className="wrap">
         <Spin />
         <div className="card">
+          {error && <div className="error-message">{error}</div>}
           <div className="card-image">
             <img src="./sea.jpg" alt="surfing" />
             <Title />
